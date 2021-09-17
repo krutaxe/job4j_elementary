7724debd18e67071957001caee3d5a1ca3364f33
Zver <krutaxe@mail.ru>
2021-09-17 21:36:53 +0300

    4.2. Заголовок в файле java [#159036]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dima Nikolaev");
+        System.out.println("17.09.2021");
+    }
+}
