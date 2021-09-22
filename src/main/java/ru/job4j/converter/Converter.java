6f5ba236712bbb8f1dfd8fa8888a68077470ca1f
Zver <krutaxe@mail.ru>
2021-09-22 13:25:58 +0300

    9. Площадь треугольника. [#175474 #205263]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,32 +2,31 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-//        float rsl = value / 70;
-//        return rsl;
-            return -1;
+        float rsl = value / 70;
+        return rsl;
+
     }
 
     public static float rubleToDollar(float value) {
-//        float rsl = value / 60;
-//        return rsl;
-        return 0;
+        float rsl = value / 60;
+        return rsl;
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(180);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollar.");
-        float in1 = 140;
-        float in2 = 180;
-        float expected1 = 2;
-        float expected2 = 3;
+        float euro = Converter.rubleToEuro(350);
+        float dollar = Converter.rubleToDollar(600);
+        System.out.println("350 rubles are " + euro + " euro.");
+        System.out.println("600 rubles are " + dollar + " dollar.");
+        float in1 = 350;
+        float in2 = 600;
+        float expected1 = 5;
+        float expected2 = 10;
         float out1 = Converter.rubleToEuro(in1);
         float out2 = Converter.rubleToDollar(in2);
         boolean passed1 = expected1 == out1;
         boolean passed2 = expected2 == out2;
-        System.out.println("140 rubles are 2. Test result : " + passed1);
-        System.out.println("180 rubles are 3 Test result : " + passed2);
+        System.out.println("350 rubles are 5. Test result : " + passed1);
+        System.out.println("600 rubles are 10. Test result : " + passed2);
 
     }
 
--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -7,18 +7,18 @@ import org.junit.Test;
 public class ConverterTest {
 
     @Test
-    public void whenConvert140RblThen2Euro() {
-        float in = 140;
-        float expected = -1;
+    public void whenConvert350RblThen5Euro() {
+        float in = 350;
+        float expected = 5;
         float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
     }
 
     @Test
-    public void whenConvert180RblThen3Dollar() {
-        float in = 180;
-        float expected = 0;
+    public void whenConvert600TblThen10Dollar() {
+        float in = 600;
+        float expected = 10;
         float out = Converter.rubleToDollar(in);
         float eps = 0.0001f;
         Assert.assertEquals(expected, out, eps);
