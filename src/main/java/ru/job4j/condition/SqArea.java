e45792d51310aaccdfb232ca4af756e8650b58ea
Zver <krutaxe@mail.ru>
2021-09-23 21:44:31 +0300

    SqArea

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -4,8 +4,8 @@ package ru.job4j.condition;
 public class SqArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
-        double l = h * k;
-        double s = l * h;
+        double w = h * k;
+        double s = w * h;
         return s;
     }
 
