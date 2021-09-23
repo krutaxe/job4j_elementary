0d0811351d961cf84ec36e8235bb9e2805192bbe
Zver <krutaxe@mail.ru>
2021-09-23 15:23:41 +0300

    4.3.6. Math.abs [#373616 #207085]

--- src/main/java/ru/job4j/condition/PointEq.java
+++ src/main/java/ru/job4j/condition/PointEq.java
@@ -3,13 +3,7 @@ package ru.job4j.condition;
 public class PointEq {
     public static boolean eq(int x1, int y1, int x2, int y2) {
         return x1 == x2 && y1 == y2;
-//        boolean eqX = x1 == x2;
-//        boolean eqY = y1 == y2;
-//        if (eqX && eqY) {
-//            return true;
-//        } else  {
-//            return false;
-//        }
+
     }
 
     public static void main(String[] args) {
