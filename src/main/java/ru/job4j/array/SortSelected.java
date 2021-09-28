b245d6390ea8333bf86b5ad34eb0d32ea00bf76b
Zver <krutaxe@mail.ru>
2021-09-28 20:30:14 +0300

    6.5.3. Сортировка выборкой. [#156320]

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -4,10 +4,11 @@ public class SortSelected {
 
     public static int[] sort(int[] data) {
         for (int i = 0; i < data.length; i++) {
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
-            data[index] = data[i];
-            data[i] = min;
+            int min = MinDiapason.findMin(data, i, data.length);
+            int index = FindLoop.indexOf(data, min, i, data.length);
+            int temp = data[i];
+            data[i] = data[index];
+            data[index] = temp;
 
         }
         return data;
--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -11,4 +11,12 @@ public class SortSelectedTest {
         int[] expected = new int[] {1, 2, 3, 4, 5};
         Assert.assertArrayEquals(expected, result);
     }
+
+    @Test
+    public void whenSortMy() {
+        int[] data = new int[] {3, 4, 1, 2, 5, 12, 9, 6, 10, 7, 11, 8};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+        Assert.assertArrayEquals(expected, result);
+    }
 }
