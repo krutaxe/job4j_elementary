a02fb77b63c5bbfb13c30b7edebf771c32a05372
Zver <krutaxe@mail.ru>
2021-10-03 13:20:22 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -6,4 +6,26 @@ public class Max {
         int result = condition ? left : right;
         return result;
     }
+
+    public static int max(int left, int right, int up) {
+        if (left > right && left > up) {
+            return left;
+        } else if (right > left && right > up) {
+            return right;
+        } else {
+            return up;
+        }
+    }
+
+    public static int max(int left, int right, int up, int down) {
+        if (left > right && left > up && left > down) {
+            return left;
+        } else if (right > left && right > up && right > down) {
+            return right;
+        } else if (up > left && up > right && up > down) {
+            return up;
+        } else {
+            return down;
+        }
+    }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -29,4 +29,25 @@ public class MaxTest {
         int expected = 3;
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void whenMax3To4To9Then9() {
+        int left = 3;
+        int right = 4;
+        int up = 9;
+        int result = Max.max(left, right, up);
+        int expected = 9;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3To4To9To20Then20() {
+        int left = 3;
+        int right = 4;
+        int up = 9;
+        int down = 20;
+        int result = Max.max(left, right, up, down);
+        int expected = 20;
+        Assert.assertEquals(result, expected);
+    }
 }
