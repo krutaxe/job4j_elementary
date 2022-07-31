5e433a11c44e7dfaee4f91c0ef41a336d5348d73
krutaxe <krutaxe@mail.ru>
2022-07-31 12:19:34 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean valid = true;
+        for (int i = 0; i < name.length(); i++) {
+            int code = name.codePointAt(i);
+            if (name.codePointAt(0) < 97 || name.codePointAt(0) > 122) {
+                valid = false;
+                break;
+            } else if ((code < 48 || code > 122) || (code > 90 && code < 97) || (code > 57 && code < 65)) {
+                if (code == 36 || code == 95) {
+                    continue;
+                }
+                valid = false;
+                break;
+            }
+        }
+        return valid;
+    }
+}
