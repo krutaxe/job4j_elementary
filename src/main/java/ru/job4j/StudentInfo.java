c13132789e5d7d69da6989d8b0df3b6294737061
krutaxe <krutaxe@mail.ru>
2023-06-22 22:16:10 +0300

    test

--- src/main/java/ru/job4j/StudentInfo.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Dima Nikolaev");
-        System.out.println("17.09.2021");
-    }
-}
